280c02a3ae7373c549a42432ab20569fb14b606c
CHAN KAI CHENG <106880309+JeterChan@users.noreply.github.com>
2023-05-09 03:07:39 +0800

    Update department.php

--- department.php
+++ department.php
@@ -3,7 +3,7 @@
     <head>
         <title>醫二三</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">        <!--到mobile裝置上仍可正常顯示,指定螢幕寬度為裝置寬度,畫面載入初始縮放比例 100% -->
+        <meta name="viewport" content="width=device-width initial-scale=1">        <!--到mobile裝置上仍可正常顯示指定螢幕寬度為裝置寬度畫面載入初始縮放比例 100% -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <link rel="stylesheet" href="/page_css/department.css">
     </head>
@@ -26,51 +26,52 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary">家庭醫學科</button>
-                    <button type="button" class="btn btn-outline-primary">內科</button>
-                    <button type="button" class="btn btn-outline-primary">外科</button>
-                    <button type="button" class="btn btn-outline-primary">兒科</button>
-                    <button type="button" class="btn btn-outline-primary">婦產科</button>
+                    <button type="button" class="btn btn-outline-primary" id="10000000000000000000000000">家庭醫學科</button>
+                    <button type="button" class="btn btn-outline-primary" id="01000000000000000000000000">內科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00100000000000000000000000">外科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00010000000000000000000000">兒科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00001000000000000000000000">婦產科</button>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary">骨科</button>
-                    <button type="button" class="btn btn-outline-primary">神經科</button>
-                    <button type="button" class="btn btn-outline-primary">神經外科</button>
-                    <button type="button" class="btn btn-outline-primary">泌尿科</button>
-                    <button type="button" class="btn btn-outline-primary">耳鼻喉科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000100000000000000000000">骨科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000010000000000000000000">神經科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000001000000000000000000">神經外科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000100000000000000000">泌尿科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000010000000000000000">耳鼻喉科</button>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary">眼科</button>
-                    <button type="button" class="btn btn-outline-primary">皮膚科</button>
-                    <button type="button" class="btn btn-outline-primary">精神科</button>
-                    <button type="button" class="btn btn-outline-primary">復健科</button>
-                    <button type="button" class="btn btn-outline-primary">麻醉科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000001000000000000000">眼科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000100000000000000">皮膚科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000010000000000000">精神科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000001000000000000">復健科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000100000000000">麻醉科</button>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary">放射診斷科</button>
-                    <button type="button" class="btn btn-outline-primary">放射腫瘤科</button>
-                    <button type="button" class="btn btn-outline-primary">解剖病理科</button>
-                    <button type="button" class="btn btn-outline-primary">臨床病理科</button>
-                    <button type="button" class="btn btn-outline-primary">整形外科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000010000000000">放射診斷科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000001000000000">放射腫瘤科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000100000000">解剖病理科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000010000000">臨床病理科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000100000">急診醫學科</button>
+                    
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary">急診醫學科</button>
-                    <button type="button" class="btn btn-outline-primary">職業醫學科</button>
-                    <button type="button" class="btn btn-outline-primary">西醫一般科</button>
-                    <button type="button" class="btn btn-outline-primary">牙醫一般科</button>
-                    <button type="button" class="btn btn-outline-primary">中醫一般科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000010000">整形外科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000001000">職業醫學科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000000100">西醫一般科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000000010">牙醫一般科</button>
+                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000000001">中醫一般科</button>
                 </div>  
             </div>
 
