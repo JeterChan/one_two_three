99a5a3b20bc901a5dd6d7a41b6938a328b3c834e
Kenzo2111 <113497726+Kenzo2111@users.noreply.github.com>
2023-05-09 03:09:20 +0800

    Update quick_doc.php

--- quick_doc.php
+++ quick_doc.php
@@ -25,3 +25,6 @@
 <a href = "department.php" button type="button" class="btn btn-outline-primary">想好了，點我找醫院</button></a>
 <a href = "choose_ill.php" button type="button" class="btn btn-outline-primary">還沒，點我查看可能需要的科別</button></a>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    </body>
+</html>
