337f4957334f8fb2686e224ff4665c7683d03d28
CHAN KAI CHENG <106880309+JeterChan@users.noreply.github.com>
2023-05-14 16:42:53 +0800

    Update department.php

--- department.php
+++ department.php
@@ -26,59 +26,56 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary" id="10000000000000000000000000">家庭醫學科</button>
-                    <button type="button" class="btn btn-outline-primary" id="01000000000000000000000000">內科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00100000000000000000000000">外科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00010000000000000000000000">兒科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00001000000000000000000000">婦產科</button>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="10000000000000000000000000">家庭醫學科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="01000000000000000000000000">內科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00100000000000000000000000">外科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00010000000000000000000000">兒科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00001000000000000000000000">婦產科</a>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary" id="00000100000000000000000000">骨科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000010000000000000000000">神經科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000001000000000000000000">神經外科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000100000000000000000">泌尿科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000010000000000000000">耳鼻喉科</button>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000100000000000000000000">骨科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000010000000000000000000">神經科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000001000000000000000000">神經外科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000100000000000000000">泌尿科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000010000000000000000">耳鼻喉科</a>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary" id="00000000001000000000000000">眼科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000100000000000000">皮膚科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000010000000000000">精神科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000001000000000000">復健科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000100000000000">麻醉科</button>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000001000000000000000">眼科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000100000000000000">皮膚科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000010000000000000">精神科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000001000000000000">復健科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000100000000000">麻醉科</a>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000010000000000">放射診斷科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000001000000000">放射腫瘤科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000100000000">解剖病理科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000010000000">臨床病理科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000100000">急診醫學科</button>
-                    
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000010000000000">放射診斷科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000001000000000">放射腫瘤科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000100000000">解剖病理科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000010000000">臨床病理科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000000100000">急診醫學科</a>
                 </div>  
             </div>
 
             <div class="row">
                 <div class="col-sm">
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000010000">整形外科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000001000">職業醫學科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000000100">西醫一般科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000000010">牙醫一般科</button>
-                    <button type="button" class="btn btn-outline-primary" id="00000000000000000000000001">中醫一般科</button>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000000010000">整形外科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000000001000">職業醫學科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000000000100">西醫一般科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000000000010">牙醫一般科</a>
+                    <a href="location.php" class="btn btn-outline-primary" role="button" aria-pressed="true" id="00000000000000000000000001">中醫一般科</a>
                 </div>  
             </div>
 
         </div>
         
-
-
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
     </body>
 </html>
